543aa782802cad48f56b802d043fec1077803992
David Maulat <david.maulat@consistec.de>
2013-02-04 17:45:44 +0100

    [SYNCBIB-166] removed useless 'server' database in mysql triggers

--- db_adapters/src/main/resources/sql/mysql_create_triggers.sql
+++ db_adapters/src/main/resources/sql/mysql_create_triggers.sql
@@ -1,18 +1,18 @@
-DROP TRIGGER IF EXISTS `server`.`%table%_after_insert`;;
-DROP TRIGGER IF EXISTS `server`.`%table%_after_update`;;
-DROP TRIGGER IF EXISTS `server`.`%table%_after_delete`;;
-CREATE TRIGGER `server`.`%table%_after_insert` AFTER INSERT ON `%table%`
+DROP TRIGGER IF EXISTS `%table%_after_insert`;;
+DROP TRIGGER IF EXISTS `%table%_after_update`;;
+DROP TRIGGER IF EXISTS `%table%_after_delete`;;
+CREATE TRIGGER `%table%_after_insert` AFTER INSERT ON `%table%`
     FOR EACH ROW
         IF (SELECT USER() NOT LIKE '%syncuser%@%') THEN
             INSERT INTO %table%%md_suffix% (%pk_md%, %flag_md%) VALUES (NEW.%pk_data%, 2)
             ON DUPLICATE KEY UPDATE %flag_md% = 1;
         END IF;;
-CREATE TRIGGER `server`.`%table%_after_update` AFTER UPDATE ON `%table%`
+CREATE TRIGGER `%table%_after_update` AFTER UPDATE ON `%table%`
     FOR EACH ROW
         IF (SELECT USER() NOT LIKE '%syncuser%@%') THEN
             UPDATE %table%%md_suffix% SET %flag_md% = 1 WHERE %pk_md% = NEW.%pk_data%;
         END IF;;
-CREATE TRIGGER `server`.`%table%_after_delete` AFTER DELETE ON `%table%`
+CREATE TRIGGER `%table%_after_delete` AFTER DELETE ON `%table%`
     FOR EACH ROW
         IF (SELECT USER() NOT LIKE '%syncuser%@%') THEN BEGIN
             -- if it's been deleted and never synced, the revision will be NULL
