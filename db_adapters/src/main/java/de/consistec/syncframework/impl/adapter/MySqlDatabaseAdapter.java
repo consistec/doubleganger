df2e47388321ed9d04a1dbc4dd3fb7fcbfea157a
David Maulat <david.maulat@consistec.de>
2013-02-04 13:26:31 +0100

    [SYNCBIB-166] reactivated test and fixed user error for Mysql triggers

--- db_adapters/src/main/java/de/consistec/syncframework/impl/adapter/MySqlDatabaseAdapter.java
+++ db_adapters/src/main/java/de/consistec/syncframework/impl/adapter/MySqlDatabaseAdapter.java
@@ -9,15 +9,15 @@ package de.consistec.syncframework.impl.adapter;
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the 
+ * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
+ *
+ * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -172,7 +172,7 @@ public class MySqlDatabaseAdapter extends GenericDatabaseAdapter {
                 String triggerRawQuery = new Scanner(getClass().getResourceAsStream(filePath))
                     .useDelimiter("\\A").next();
 
-                String triggerQuery = triggerRawQuery.replaceAll("%syncuser%", SYNC_USER);
+                String triggerQuery = triggerRawQuery.replaceAll("%syncuser%", username);
                 triggerQuery = triggerQuery.replaceAll("%table%", tableName);
                 triggerQuery = triggerQuery.replaceAll("%md_suffix%", CONF.getMdTableSuffix());
                 triggerQuery = triggerQuery.replaceAll("%pk_data%", getPrimaryKeyColumn(tableName).getName());
