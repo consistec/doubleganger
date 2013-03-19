1dd43eb8a35ecfd1383e266b250f62cf55230b1f
David Maulat <david.maulat@consistec.de>
2013-03-19 11:48:50 +0100

    SYNCBIB-204 - split syncframework into separate repos? changed repository structure (WIP)

--- common/src/main/java/de/consistec/doubleganger/common/i18n/MessageReader.java
+++ common/src/main/java/de/consistec/doubleganger/common/i18n/MessageReader.java
@@ -9,15 +9,15 @@ package de.consistec.doubleganger.common.i18n;
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the 
+ * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
+ *
+ * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
@@ -41,7 +41,6 @@ import java.util.Map;
 public final class MessageReader {
 
 //<editor-fold defaultstate="expanded" desc=" Class fields " >
-
     /**
      * Default conveyor is specified as field to avoid unnecessary searches through the map.
      */
@@ -79,16 +78,5 @@ public final class MessageReader {
             throw new IllegalArgumentException("Provided enumeration is not an CAL10n enum!");
         }
     }
-
-//    This method will be needed in future, for various bundles.
-//    public static IMessageConveyor lookup(Locale locale) {
-//        IMessageConveyor conveyor = cache.get(locale);
-//        if (conveyor == null) {
-//            conveyor = new MessageConveyor(locale);
-//            cache.put(locale, conveyor);
-//        }
-//        return conveyor;
-//    }
-
 //</editor-fold>
 }
